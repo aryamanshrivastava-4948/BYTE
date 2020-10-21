5de26320721a898f2d63f253657181d2009b176d
Aryaman <shrivastavaaryaman0702@gmail.com>
2020-10-21 23:27:36 +0530

    initial commit

--- app/src/main/java/com/arylist/abyte/MainActivity.kt
+++ app/src/main/java/com/arylist/abyte/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.arylist.abyte
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import android.widget.Toast.*
@@ -13,27 +14,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        savebut.setOnClickListener {
+        savebut.setOnClickListener{
             val id = IdField.text.toString()
             val name = nameField.text.toString()
             val age = ageField.text.toString()
-            val userData = UserData(name, age)
+            val userData = UserData(name,age)
             val ref = FirebaseDatabase.getInstance().getReference("${id}/")
             ref.setValue(userData)
                 .addOnSuccessListener {
-                    Toast.makeText(this, "Data Saved Successfully", LENGTH_SHORT).show()
+                    Toast.makeText(this,"Data Saved Successfully",Toast.LENGTH_SHORT).show()
+
                 }
-
-                .addOnFailureListener{
-                  Toast.makeText(this, "Data Could Not be Saved", LENGTH_SHORT).show()
+                .addOnFailureListener {
+                    Toast.makeText(this,"Data Could not be saved",Toast.LENGTH_SHORT).show()
                 }
-
-
         }
     }
 }
 
-data class UserData(val name:String,val age:String )
+data class UserData(val name: String, val age: String)
 {
-    constructor():this("","")
+    constructor(): this("","")
 }
